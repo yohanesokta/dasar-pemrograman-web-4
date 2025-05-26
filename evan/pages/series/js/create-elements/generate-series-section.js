e5637f9bf6db65008faa2b6d0ffd091670c594e7
evanalifian <evanalifian@gmal.com>
2025-05-27 00:59:17 +0700

    create series page

---
+++ evan/pages/series/js/create-elements/generate-series-section.js
@@ -0,0 +1,24 @@
+import { fetchMovies } from "../fetch-movies.js";
+
+(async function GenerateSeriesSection() {
+  const data = await fetchMovies()
+  const movies = data.results.slice()
+  const main = document.getElementById("main");
+  main.innerHTML = `
+    <div class="movie_posters_section">
+      <h1>Series Movie</h1>
+      <div class="movie_poster_grid">
+        ${movies
+          .map(
+            (movie) => `
+          <figure class="backdrop" title="${movie.name}">
+            <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" alt="${movie.name} backdrop">
+            <figcaption>${movie.name}</figcaption>
+          </figure>
+        `
+          )
+          .join("")}
+      </div>
+    </div>
+  `;
+})()
