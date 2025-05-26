d21c13fa1d4b67b7b2898a6c9a14d459d2417837
evanalifian <evanalifian@gmal.com>
2025-05-26 23:44:49 +0700

    rename file

--- evan/js/dom.js
+++
@@ -1,5 +0,0 @@
-import Sidebar from "./create-elements/sidebar.js";
-import GenerateHomeContent from "./create-elements/generate-home-content.js";
-
-Sidebar();
-// GenerateHomeContent()
---
+++ evan/js/layout-dom.js
@@ -0,0 +1,3 @@
+import Sidebar from "./create-elements/sidebar.js";
+
+Sidebar();
