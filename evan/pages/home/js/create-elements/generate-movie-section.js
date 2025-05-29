2c2741f75e3f78e3140041b4c0ab1eb6730ee4a7
Evan Alifian <95698228+evanalifian@users.noreply.github.com>
2025-05-29 10:39:34 +0700

    updated evan (#27)
    * create landing page
    
    * update css we need
    
    * update home page and add theme toggle
    
    * file is deleted or moved
    
    * rename component class name
    
    * remove selector '.movie_posters_section h2'
    
    * update js
    
    * move file to specific page
    
    * delete unused css file
    
    * rename file
    
    * create series page
    
    * add backdrop card element class name
    
    * run generateHomeContent()
    
    * update backdrop card component class name
    
    * add backdrop_cover for image
    
    * change minmax() value and gap value
    
    * update sidebar list menu
    
    * move to /pages/@Discover path
    
    * create movie and tv/series page
    
    * update a couple line code
    
    ---------
    
    Co-authored-by: evanalifian <evanalifian@gmal.com>

---
+++ evan/pages/home/js/create-elements/generate-movie-section.js
@@ -0,0 +1,26 @@
+import { fetchMovies } from "../fetch-movies.js";
+
+export default async function GenerateMovieSection(type, title) {
+  const data = await fetchMovies(type)
+  const movies = data.results.slice(0, 4)
+  return `
+    <section class="movie_posters_section">
+      <h2>${title}</h2>
+      <div class="movie_poster_grid">
+        ${movies
+          .map(
+            (movie) => `
+            <figure class="poster_card" title="${movie.title}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+              <figcaption>
+                <span class="poster_card_title">${movie.title}</span>
+                <span class="poster_card_description">${movie.release_date}</span>
+              </figcaption>
+            </figure>
+            `
+          )
+          .join("")}
+      </div>
+    </section>
+  `;
+}
