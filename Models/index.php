4fc420aa6176b037b8d2493b394744123f6ea89c
Yohanes Oktanio <yohanesoktayt72@gmail.com>
2025-06-02 12:08:27 +0700

    feat: database

--- Models/index.php
+++ Models/index.php
@@ -10,4 +10,9 @@ try {
     die("Environment variable validation failed: " . $e->getMessage());
 }
 
-echo $_ENV['API_KEY'];
+var_dump($_ENV["DB_HOST"]);
+$conn = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+
+if ($conn->connect_error) {
+  die("Koneksi gagal: " . $conn->connect_error);
+}
