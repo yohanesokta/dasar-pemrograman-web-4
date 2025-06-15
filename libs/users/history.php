922d2bfdc214b9ee62ed1bed623ac388a6c9ff75
yohanesokta <yohanesoktayt72@gmail.com>
2025-06-15 17:42:51 +0700

    change pw

--- libs/users/history.php
+++ libs/users/history.php
@@ -1,5 +1,5 @@
 <?php
-    $stmt = $conn->prepare("SELECT * FROM history WHERE username = ?");
+    $stmt = $conn->prepare("SELECT * FROM history WHERE username = ? ORDER BY update_at DESC");
     $stmt->bind_param("s", $user_data["username"]);
     $stmt->execute();
     $result = $stmt->get_result();
